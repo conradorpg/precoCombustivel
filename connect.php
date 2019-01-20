50ed96aa6646c403f2df7bf6e8b3ab495db3f885
conradorpg <45433833+conradorpg@users.noreply.github.com>
2019-01-20 18:54:31 -0200

    Create connect.php

---
+++ connect.php
@@ -0,0 +1,14 @@
+<?php
+
+// <-- Dados da Conexão -->
+
+    $servidor = "localhost";
+    $usuario = "seu_usuario";
+    $senha = "sua_senha";
+    $dbname = "precocombustivel";
+
+// <-- Criar Conexão -->
+
+$conn = mysqli_connect($servidor, $usuario, $senha, $dbname);
+
+?>
