382c9589f9d01927dafa50693bb7695f46ded79d
conradorpg <45433833+conradorpg@users.noreply.github.com>
2019-01-20 18:53:13 -0200

    Create index.php

---
+++ index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="utf-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<meta name="description" content="Incluir dados de XML para SQL">
+<meta name="author" content="Conrado Gomes">
+
+<title>Importa dados Excel</title>
+
+</head>
+<body>
+
+<div>
+    <h3>Upload XML</h3>
+    <form method="post" action="process.php" enctype="multipart/form-data">
+        <label>Arquivo</label>
+        <input type="file" name="arquivo"><br><br>
+        <input type="submit" value="Enviar">
+    </form>
+
+</div>
+
+</body>
+</html>
